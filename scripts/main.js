13bd6297b14213cc02276f59d177a48f11cef71f
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2020-05-06 15:08:49 +0200

    resized background image
    Signed-off-by: Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>

--- scripts/main.js
+++ scripts/main.js
@@ -124,31 +124,29 @@ x.remove();})
 //żeby ta animka dobrze działała trza by wziąć oba elementy poza footer absolutnie animka sprawi że linki będą latać po ekranie
 
 
-$(document).ready(function(){
-  console.log('script');
+// $(document).ready(function(){
+//   console.log('script');
  
-  animateDiv('.ufo');
+//   animateDiv('.ufo');
   
-});
+// });
 
-function makeNewPosition(){
+// function makeNewPosition(){
   
-  // Get viewport dimensions (remove the dimension of the div)
-  var h = $(window).height() - 50;
-  var w = $(window).width() - 50;
+//   var h = $(window).height() - 50;
+//   var w = $(window).width() - 50;
   
-  var nh = Math.floor(Math.random() * h);
-  var nw = Math.floor(Math.random() * w);
+//   var nh = Math.floor(Math.random() * h);
+//   var nw = Math.floor(Math.random() * w);
   
-  return [nh,nw];    
+//   return [nh,nw];    
   
-}
+// }
 
-function animateDiv(myclass){
-  var newq = makeNewPosition();
-  console.log('myclass',myclass);
-  $(myclass).animate({ top: newq[0], left: newq[1] }, 10000,   function(){
-    animateDiv(myclass);        
-  });
+// function animateDiv(myclass){
+//   var newq = makeNewPosition();
+//   $(myclass).animate({ top: newq[0], left: newq[1] }, 10000,   function(){
+//     animateDiv(myclass);        
+//   });
   
-};
+// };
